843f4a708768087fe2f38e3929d27f7d3bc4015d
Anujram07 <anujramraika12@gmail.com>
2025-06-11 11:19:13 +0530

    BANK

--- BankAccount.java
+++ BankAccount.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 
 class Transaction {
     String type;
@@ -19,6 +23,7 @@ class Account {
     private String Account_Holder;
     private int AccountNo;
     private double Bank_Balance;
+
     ArrayList<Transaction> transaction = new ArrayList<>();
 
     public Account(String name, int AccountNo, double Bank_Balance) {
@@ -65,6 +70,55 @@ class Account {
     public void checkBalance() {
         System.out.println("Current Balance: Rs. " + Bank_Balance);
     }
+
+
+    public void Loan(double amount){
+        if(Bank_Balance>1000){
+            System.out.println("Loan Granted");
+        }
+        else{
+             System.out.println("you Sholud have minimum account of $10000 in your account");
+        }
+       
+    }
+
+
+    public int getAccountNo(){
+        return AccountNo;
+    }
+
+    public String getname(){
+        return Account_Holder;
+    }
+
+    public void saveaccount(String name){
+         try {
+            File file = new File(name);
+            if(file.createNewFile()){
+                System.out.println("File Created" + file.getName());
+            } else {
+                System.out.println("File already Exist.");
+            }
+            
+            
+        } catch (IOException e) {
+            System.out.println("An Error occurred");
+            e.printStackTrace();
+        }
+    }
+
+   public void savetransaction(double amount) {
+    try {
+        FileWriter writer = new FileWriter("new.txt", true); // append mode
+        writer.write("Transaction Amount: Rs. " + amount + "\n");
+        writer.close();
+        System.out.println("Successfully Written!");
+    } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+    }
+}
+
 }
 
 public class BankAccount {
@@ -72,22 +126,25 @@ public class BankAccount {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello! Welcome to Dhani Money Bank");
-        System.out.println("----------------------------------");
+       
+       
         System.out.print("Enter Your Name: ");
         String name = in.nextLine();
+        System.out.println("Hello! Welcome Dear "+name+" Sir to Dhani Money Bank");
+        System.out.println("-------------------------------------------------");
 
         // Create account with dummy account number and initial balance
         account = new Account(name, 1001, 0.0);
 
         int choice = 0;
-        while (choice != 5) {
+        while (choice != 6) {
             System.out.println("\nMenu:");
             System.out.println("1. Deposit");
             System.out.println("2. Withdraw");
             System.out.println("3. Check Balance");
             System.out.println("4. Transaction History");
-            System.out.println("5. Exit");
+            System.out.println("5. Apply for loan");
+            System.out.println("6. Exit");
 
             System.out.print("Please Enter Your Choice: ");
             if (in.hasNextInt()) {
@@ -99,11 +156,13 @@ public class BankAccount {
             }
 
             switch (choice) {
-                case 1:
-                    System.out.print("Enter amount to deposit: ");
-                    double dep = in.nextDouble();
-                    account.deposit(dep);
-                    break;
+            case 1:
+                  System.out.print("Enter amount to deposit: ");
+                  double dep = in.nextDouble();
+                 account.deposit(dep);
+                  account.saveaccount(name);
+                   account.savetransaction(dep);
+                  break;
                 case 2:
                     System.out.print("Enter amount to withdraw: ");
                     double with = in.nextDouble();
@@ -115,7 +174,15 @@ public class BankAccount {
                 case 4:
                     account.transaction();
                     break;
+
                 case 5:
+                    System.out.print("Enter amount to deposit: ");
+                    double lon = in.nextDouble();
+                    account.Loan(lon);
+                    break;
+                    
+                case 6:
+                 
                     System.out.println("Thank you for using the Bank App.");
                     break;
                 default:
